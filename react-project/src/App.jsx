85343b4a3f1cb07dd8ed825a71023a3e0f9d7942
tlovegrove3 <tlovegrove3@ivytech.edu>
2025-07-12 18:13:18 -0400

    State management applied. You can close or open a course

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,27 +1,74 @@
+import { useState } from 'react'
 import './App.css'
+import three from '/images/books.png'
+
+const [ , , third] = ['tardigrade', 'dolphin', 'dog'];
+console.log(third);
+
 
 let language = 'JavaScript';
 let symbol = '🌚';
 
-function Header(props) {
+function Header({ name, year }) {
   return (
     <header>
-      <h1>{props.name}'s React App</h1>
-      <p>Copyright {props.year}</p>
+      <h1>{name}'s React App</h1>
+      <p>Copyright {year}</p>
     </header>
   );
 }
 
-function App() {
+const courses = [
+  'SDEV255',
+  'SDEV272',
+  'IVYT111'
+];
 
+const courseObjects = courses.map((course, i) => ({
+    id: i,
+    title: course
+}));
+
+function Main({ courses }) {
+  return (
+    <>
+    <div>
+      <h2>Welcome to the Course List</h2>
+    </div>
+    <main>
+      <img src={three} alt="Smiling face"/>
+      <div>
+        <h2>
+          {language} is a good language to learn {symbol}<br />
+          List of courses:
+        </h2>
+        <ol>
+          {courses.map((course) => (
+            <li key={course.id} style=
+            {{
+              listStyleType: 'none'
+            }}
+            >{course.title}</li>
+          ))}
+        </ol>
+      </div>
+    </main>
+    </>
+  );
+}
+
+
+function App() {
+  const [status, setStatus] = useState(true);
   return (
     <div>
+      <h1>The course is currently {status ? "Open" : "Closed"}</h1>
+      <button onClick={() =>  setStatus(!status)}>
+        { status ? 'Close' : 'Open' } Course
+      </button>
+
       <Header name="TJ" year={2025}/>
-      <main>
-        <h2>
-          {language} is a good language to learn {symbol}
-        </h2>
-      </main>
+        <Main courses={courseObjects} openStatus={status}/>
     </div>
     
   )
